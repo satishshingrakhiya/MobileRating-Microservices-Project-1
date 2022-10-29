65d83efe122e5ac6e6a7481967a086c3df9c828b
Satish Shingrakhiya <satish.shingrakhiy@gmail.com>
2022-10-30 01:20:20 +0530

    05. Use WebClient

--- mobile-catalog-service/src/main/java/com/microservices1/mobilecatalogservice/controller/MobileCatalogController.java
+++ mobile-catalog-service/src/main/java/com/microservices1/mobilecatalogservice/controller/MobileCatalogController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -20,6 +21,10 @@ public class MobileCatalogController {
 
     @Autowired
     RestTemplate restTemplate;
+
+    @Autowired
+    WebClient.Builder builder;
+
     @RequestMapping("{userId}")
     public List<CatalogItem> getCatalog(@PathVariable String userId){
 
@@ -30,6 +35,15 @@ public class MobileCatalogController {
         return ratings.stream().map(
                 rating -> {
                     Mobile mobile = restTemplate.getForObject("http://localhost:8082/mobile/" + rating.getModelName(), Mobile.class);
+                    /*
+                    // Using Webclient
+                    Mobile mobile = builder.build()
+                            .get()
+                            .uri("http://localhost:8082/mobile/" + rating.getModelName())
+                            .retrieve()
+                            .bodyToMono(Mobile.class)
+                            .block();
+                     */
                     return new CatalogItem(mobile.getModelName(), mobile.getCompanyName(), rating.getRating());
                 })
                 .collect(Collectors.toList());
